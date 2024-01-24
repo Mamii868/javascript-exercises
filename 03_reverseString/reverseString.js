c0fa40fae2091bcb734302a52c80781faae5a89c
Mamii868 <marcmotenii@gmail.com>
2024-01-24 17:58:20 -0600

    Solved problem
    turned string into array, use for loop to reverse array, then join array and turn to string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse = [];
+    let splitString = string.split("");
+    for (let i = splitString.length - 1; i >= 0; i--) {
+        reverse.push(splitString[i])
+    }
+    reverse = reverse.join("");
+    return reverse
 };
 
 // Do not edit below this line
