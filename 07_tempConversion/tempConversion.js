a8e23ee8ecfdd08fb49d85c22989635f00d503ae
Mamii868 <marcmotenii@gmail.com>
2024-01-30 21:10:44 -0600

    Solved
    converts temperatures using correct formula, then checks to see if a decimal place exists in the number to round it to one place or not.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celcius = ((temp - 32) * (5 / 9))
+  if (Number.isInteger(celcius)) {
+    return Number(celcius)
+  } else if (!Number.isNaN(celcius)){
+    celcius = celcius.toFixed(1)
+    return Number(celcius)
+  } else {
+    celcius = "ERROR"
+    return "ERROR"
+  } 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = ((temp * (9 / 5)) + 32)
+  if (Number.isInteger(fahrenheit)){
+    return Number(fahrenheit)
+  } else if (!Number.isNaN(fahrenheit)){
+    fahrenheit = fahrenheit.toFixed(1)
+    return Number(fahrenheit)
+  } else {
+    fahrenheit = "ERROR"
+    return "ERROR"
+  }
 };
 
 // Do not edit below this line
