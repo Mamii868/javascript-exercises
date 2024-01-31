2b05e67aa6939ce7f367a0c13caa61cd3e0b79ad
Mamii868 <marcmotenii@gmail.com>
2024-01-30 20:32:19 -0600

    Solved
    Set the function to take the two numbers and loop through the numbers in between, adding them together. If the input is not a number or is negative, returns as an error

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(inum, fnum) {
+    let sum = 0;
+    if (inum < fnum) {
+        for (let i = inum; i <= fnum; i++) {
+            sum += i;
+        }
+    } else if (inum > fnum) {
+        for (let i = inum; i >= fnum; i--) {
+            sum += i;
+        }
+    } else{
+        sum = "ERROR"
+    }
 
+    if (sum < 0 || !Number.isInteger(inum) || !Number.isInteger(fnum)) {
+        sum = "ERROR"
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
