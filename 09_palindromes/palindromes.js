092d99551850d806e63ae1d2cd56046980b085b3
Mamii868 <marcmotenii@gmail.com>
2024-03-01 17:53:30 -0600

    Solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+const arr = string.toLowerCase().split("")
+const filteredArr = arr.filter((item) => !item.match(/[^\w\s]+|\s/))
+const reversedArr = filteredArr.toReversed()
+if (filteredArr.toString() === reversedArr.toString()) {
+    return true
+} else {
+    return false
+}
 };
 
 // Do not edit below this line
