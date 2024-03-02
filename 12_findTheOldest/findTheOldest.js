ebe1bbdc4523f88460a6feabc14a5a384ba112a1
Mamii868 <marcmotenii@gmail.com>
2024-03-01 20:31:34 -0600

    solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  function findAge(yearOfBirth, yearOfDeath) {
+    if (yearOfBirth && yearOfDeath) {
+        return yearOfDeath - yearOfBirth
+    } else {
+        return 2024 - yearOfBirth;
+    }
+  }
+  const ageArray = people.map((person) => ({
+    name: person.name,
+    age: findAge(person.yearOfBirth, person.yearOfDeath),
+  }));
 
+  ageArray.sort((a, b) => {
+      return b.age - a.age
+  })
+  return ageArray[0];
 };
 
 // Do not edit below this line
