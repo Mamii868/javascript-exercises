e7bc112a20c97892b227506aea5905cc4ccb573a
Mamii868 <marcmotenii@gmail.com>
2024-03-01 18:30:59 -0600

    Solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const arr = [0, 1]; 
+    num = Number(num)
+    if (num === 0 ) {
+        return 0;
+    } else if (num < 0 || isNaN(num)) {
+        return "OOPS"
+    } 
+    for (let i = 1; i < num; i++) {
+        arr.push((arr[arr.length - 2] + arr[arr.length - 1]))
+    }
+    return arr[arr.length - 1]
 };
 
 // Do not edit below this line
